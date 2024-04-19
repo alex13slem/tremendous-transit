3258e5c311d216ccd0c15f16cd3dd07d6d648972
alex13slem <a.s.scherba@yandex.by>
2024-04-19 14:59:38 +0300

    asd

--- src/components/ui/MobileMenuToggler.svelte
+++ src/components/ui/MobileMenuToggler.svelte
@@ -1,50 +1,50 @@
 <script lang="ts">
+  import Icon from '@iconify/svelte';
   import { isOpen } from '../../store/site-menu';
 </script>
 
 <button class:open={$isOpen} on:click={() => isOpen.set(!$isOpen)}>
-  <span />
-  <span />
-  <span />
+  {#if $isOpen}
+    <Icon icon="line-md:menu-to-close-alt-transition" />
+  {:else}
+    <Icon icon="line-md:close-to-menu-alt-transition" />
+  {/if}
 </button>
 
 <style lang="scss">
   button {
-    align-self: center;
-    position: relative;
-    width: 20px;
-    height: 22px;
     background: transparent;
     border: none;
     padding: 0;
     cursor: pointer;
+    font-size: 22px;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
+    // display: flex;
+    // flex-direction: column;
+    // justify-content: space-around;
   }
 
-  span {
-    background-color: #fff;
-    border-radius: 2px;
-    transition: 0.3s cubic-bezier(0.8, 0.5, 0.2, 1.4);
-    width: 100%;
-    height: 2px;
-    transition-duration: 500ms;
-    transform-origin: right;
-  }
-  span:nth-child(2) {
-    opacity: 1;
-  }
-  button.open span:nth-child(1) {
-    transform: rotate(-45deg);
-  }
-  button.open span:nth-child(2) {
-    opacity: 0;
-  }
-  button.open span:nth-child(3) {
-    transform: rotate(45deg);
-  }
+  // span {
+  //   background-color: #fff;
+  //   border-radius: 2px;
+  //   transition: 0.3s cubic-bezier(0.8, 0.5, 0.2, 1.4);
+  //   width: 100%;
+  //   height: 2px;
+  //   transition-duration: 500ms;
+  //   transform-origin: right;
+  // }
+  // span:nth-child(2) {
+  //   opacity: 1;
+  // }
+  // button.open span:nth-child(1) {
+  //   transform: rotate(-45deg);
+  // }
+  // button.open span:nth-child(2) {
+  //   opacity: 0;
+  // }
+  // button.open span:nth-child(3) {
+  //   transform: rotate(45deg);
+  // }
 
   @media (min-width: 1024px) {
     button {
