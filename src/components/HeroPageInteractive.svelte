93a5cc4a3204c1b1b677e3a2e097a81ee5488def
alex13slem <a.s.scherba@yandex.by>
2024-05-30 14:59:40 +0300

    fix

--- src/components/HeroPageInteractive.svelte
+++ src/components/HeroPageInteractive.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
+  import { useragent } from '@sveu/browser';
   import { isOpen } from './modals/DevServModal';
   import BtnFirm from './ui/BtnFirm.svelte';
+  import { navigate } from 'astro:transitions/client';
+
+  const { mobile } = useragent();
 </script>
 
 <div class="root">
-  <BtnFirm on:click={() => ($isOpen = true)}>Создать новый мир</BtnFirm>
+  <BtnFirm
+    on:click={() => {
+      if ($mobile) navigate('/development/form');
+      else $isOpen = true;
+    }}>Создать новый мир</BtnFirm
+  >
 </div>
 
 <style lang="scss">
